1acb4f3d3477a13252bc2f4b63ef009bf213a929
GauravArbat <gauravrarbat@gmail.com>
2025-08-23 02:30:44 +0530

    Update vite.config.js
    for deployment

--- vite.config.js
+++ vite.config.js
@@ -6,16 +6,16 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5000',
-      '/upload': 'http://localhost:5000',
-      '/clean': 'http://localhost:5000',
-      '/report': 'http://localhost:5000',
-      '/download_data': 'http://localhost:5000',
-      '/admin/user': 'http://localhost:5000',
-      '/profile': 'http://localhost:5000',
-      '/avatars': 'http://localhost:5000',
-      '/health': 'http://localhost:5000',
-      '/test-auth': 'http://localhost:5000'
+      '/api': 'https://asdp-banckend.onrender.com/',
+      '/upload': 'https://asdp-banckend.onrender.com',
+      '/clean': 'https://asdp-banckend.onrender.com/',
+      '/report': 'https://asdp-banckend.onrender.com/',
+      '/download_data': 'https://asdp-banckend.onrender.com/',
+      '/admin/user': 'https://asdp-banckend.onrender.com/',
+      '/profile': 'https://asdp-banckend.onrender.com/',
+      '/avatars': 'https://asdp-banckend.onrender.com/',
+      '/health': 'https://asdp-banckend.onrender.com/',
+      '/test-auth': 'https://asdp-banckend.onrender.com/'
     }
   }
 })
